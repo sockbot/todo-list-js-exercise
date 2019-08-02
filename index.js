56553d5f5c35479c0e5326c6f31095f04e314a67
sockbot <github@sockbot.com>
2019-08-02 21:15:01 +0000

    refactored code to OOP

--- index.js
+++ index.js
@@ -1,31 +1,41 @@
 // Arrays to keep track of each task's state
-const taskTitles = [];
-const taskComplete = [];
+// const taskTitles = [];
+// const taskComplete = [];
+
 
 // Create a new task by adding to the arrays
 // A new task will be created as incomplete
-function newTask(title) {
-  taskTitles.push(title);
-  taskComplete.push(false);
+function newTask(title, description) {
+  const task = {
+    title: title,
+    description: description,
+    complete: false,
+    // Print the state of a task to the console in a nice readable way
+    logTaskState: function() {
+      // const title = taskTitles[taskIndex];
+      // const complete = taskComplete[taskIndex];
+      console.log(`${this.title} has${this.complete ? " " : " not "}been completed`);
+    },
+    // Mark a task as complete by setting the task's status in the `taskComplete` array to `true`
+    completeTask: function() {
+      this.complete = true;
+    }
+
+  };
+  return task;
 }
 
-// Mark a task as complete by setting the task's status in the `taskComplete` array to `true`
-function completeTask(taskIndex) {
-  taskComplete[taskIndex] = true;
-}
 
-// Print the state of a task to the console in a nice readable way
-function logTaskState(taskIndex) {
-  const title = taskTitles[taskIndex];
-  const complete = taskComplete[taskIndex];
-  console.log(`${title} has${complete ? " " : " not "}been completed`);
-}
 
 // DRIVER CODE BELOW
 
-newTask("Clean Cat Litter"); // task 0
-newTask("Do Laundry"); // task 1
+const task1 = newTask("Clean Cat Litter", "Take all the poo out of the litter box"); // task 0
+const task2 = newTask("Do Laundry", "Wash, dry and fold laundry"); // task 1
+const tasks = [task1, task2]
 
-logTaskState(0); // Clean Cat Litter has not been completed
-completeTask(0);
-logTaskState(0); // Clean Cat Litter has been completed
+
+task1.logTaskState(); // Clean Cat Litter has not been completed
+task1.completeTask();
+task1.logTaskState(); // Clean Cat Litter has been completed
+
+console.log(tasks);
